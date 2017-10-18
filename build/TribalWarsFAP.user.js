c4ca419a399170d4acabbb0728ac8d32c07111fb
bartosz444 <bartoszstefanek@interia.pl>
2017-10-18 02:02:09 +0200

    Update TribalWarsFAP.user.js

--- build/TribalWarsFAP.user.js
+++ build/TribalWarsFAP.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name          TW Farm Assistant Plus
+// @name          TW Farm Assistant Plus2
 // @namespace     https://github.com/sindresorhus/cssprettifier-userscript
 // @version       0.9.1
 // @description   One click farm helper for Tribal Wars.
 // @author        TheVTM
 // @require       https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.js
-// @match         *://*.tribalwars.com.br/game.php?*screen=am_farm*
+// @match https://pl122.plemiona.pl/*
 // @downloadURL   https://github.com/thevtm/Tribal-Wars-Farm-Assistant-Plus/raw/master/build/TribalWarsFAP.user.js
 // @grant         GM_getValue
 // @grant         GM_setValue
